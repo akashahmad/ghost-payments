df57b44a1c248cdc912209a2969f38d893b6f35d
Cathy Minutillo <cathyminutillo@Cathys-MBP.attlocal.net>
2020-01-22 11:47:29 -0800

    deleted content in app.js file

--- App.js
+++ App.js
@@ -7,34 +7,7 @@ import LogSignImageLogo from './assets/img/log-sign-logo.png';
 
 const App: () => React$Node = () => {
 	return (
-    	<View style={globalStyles.heroIsFullHeight}>
-      		<StatusBar barStyle="light-content" />
-			<SafeAreaView>
-				<ScrollView>
-					<View style={globalStyles.heroCenter} >
-						<View>	
-							<Image 
-								source={LogSignImageLogo}
-								style={globalStyles.LogInSignUpLogo} 
-							/>
-						</View>
-						<View>
-							<TouchableOpacity 
-								style={globalStyles.logInButton}
-								onPress={() => navigate('LogIn')}
-							>
-								<Text style={globalStyles.logInButtonText}>LOG IN</Text>
-							</TouchableOpacity>
-						</View>
-						<View style={globalStyles.moveDown}>
-							<TouchableOpacity style={globalStyles.logInButtons}>
-								<Text style={globalStyles.logInButtonText}>SIGN UP</Text>
-							</TouchableOpacity>
-						</View>
-					</View>
-				</ScrollView>
-			</SafeAreaView>
-		</View>
+    	
   	);
 };
 
