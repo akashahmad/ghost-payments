6f5d5ae1680fc50f1551c87bd39cc3351f464876
Cathy Minutillo <cathyminutillo@Cathys-MBP.attlocal.net>
2020-01-22 15:44:35 -0800

    ui changes

--- App.js
+++ App.js
@@ -1,9 +1,4 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, Image, Button, TouchableOpacity } from 'react-native';
-import { Header, Colors } from 'react-native/Libraries/NewAppScreen';
-import Navigator from './routes/AuthStack';
-import globalStyles from './styles/global';
-import LogSignImageLogo from './assets/img/log-sign-logo.png';
 
 const App: () => React$Node = () => {
 	return (
