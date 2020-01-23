9519f6a8ba371f426ffa7a4d4ec18dd93cc5fc16
Cathy Minutillo <cathyminutillo@Cathys-MBP.attlocal.net>
2020-01-22 23:57:07 -0800

    auth flow finished

--- screens/Auth.js
+++ screens/Auth.js
@@ -3,37 +3,21 @@ import { SafeAreaView, ScrollView, View, Text, StatusBar, Image, Button, Touchab
 import globalStyles from '../styles/global';
 import LogSignImageLogo from '../assets/img/log-sign-logo.png';
 
-const Auth: () => React$Node = () => {
-	return (
-    	<View style={globalStyles.heroIsFullHeight}>
-      		<StatusBar barStyle="light-content" />
-			<SafeAreaView>
-				<ScrollView>
-					<View style={globalStyles.heroCenter} >
-						<View>	
-							<Image 
-								source={LogSignImageLogo}
-								style={globalStyles.LogInSignUpLogo} 
-							/>
-						</View>
-						<View>
-							<TouchableOpacity 
-								style={globalStyles.logInButton}
-								onPress={() => navigate('LogIn')}
-							>
-								<Text style={globalStyles.logInButtonText}>LOG IN</Text>
-							</TouchableOpacity>
-						</View>
-						<View style={globalStyles.moveDown}>
-							<TouchableOpacity style={globalStyles.logInButtons}>
-								<Text style={globalStyles.logInButtonText}>SIGN UP</Text>
-							</TouchableOpacity>
-						</View>
-					</View>
-				</ScrollView>
-			</SafeAreaView>
+function Auth() {
+    return (
+		<View style={globalStyles.newHero}>
+			<Image 
+				source={LogSignImageLogo}
+				style={globalStyles.LogInSignUpLogo} 
+			/>	
+			<TouchableOpacity style={globalStyles.logInButton}>
+				<Text style={globalStyles.logInButtonText}>LOG IN</Text>
+			</TouchableOpacity>	
+			<TouchableOpacity style={globalStyles.logInButtons}>
+				<Text style={globalStyles.logInButtonText}>SIGN UP</Text>
+			</TouchableOpacity>	
 		</View>
-  	);
-};
+	)
+}
 
 export default Auth;
