680054fdd8896c5e460c8f1509927b52287485b4
akashahmad <engr.akashahmad@gmail.com>
2020-01-24 04:12:08 +0500

    login setting

--- screens/LogIn/LogIn.js
+++ screens/LogIn/LogIn.js
@@ -3,24 +3,87 @@ import {SafeAreaView, ScrollView, View, Text, StatusBar, Image, Button, StyleShe
 import {TouchableOpacity, TextInput} from 'react-native';
 import globalStyles from '../../styles/global';
 import LogSignImageLogo from '../../assets/img/log-sign-logo.png';
+import {showMessage} from "react-native-flash-message";
+import firebase from "../../utils/firebase";
 
 function LogIn(props) {
     let {dispatch, setShow} = props;
+    const [email, setEmail] = useState(null);
+    const [emailEmpty, setEmailEmp] = useState(false);
+    const [password, setPass] = useState(null);
+    const [passwordEmpty, setPassEmp] = useState(null);
+    const [message, setMessage] = useState(null);
+
+    signIn = () => {
+        setEmailEmp(!email);
+        setPassEmp(!password);
+        if (!email || !password) {
+            return;
+        }
+        dispatch({
+            type: "SET_LOADER",
+            payload: true
+        });
+        setMessage('Signing in...');
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(confirmResult => {
+                firebase.auth().currentUser.getIdTokenResult().then((token) => {
+                    dispatch({
+                        type: "SET_LOADER",
+                        payload: false
+                    });
+                });
+                setMessage(null);
+            })
+            .catch(error => {
+                dispatch({
+                    type: "SET_LOADER",
+                    payload: false
+                });
+                setMessage(`Something went wrong please try again!`);
+            });
+    };
+    setEmailVal = (value) => {
+        setEmail(value);
+        setEmailEmp(!value);
+    };
+    setPassVal = (value) => {
+        setPass(value);
+        setPassEmp(!value);
+    };
     return (
         <SafeAreaView style={styles.signUpView}>
             <Image
                 source={LogSignImageLogo}
                 style={globalStyles.LogInSignUpLogoss}
             />
-            <TextInput placeholder={'Email address'} style={styles.signUpInputFields}/>
-            <TextInput placeholder={'Password'} style={styles.signUpInputFieldss}/>
-            <TouchableOpacity onPress={() => dispatch({type: "SET_LOGGEDIN", payload: true})}
+            <TextInput placeholder={'Email address'}
+                       autoFocus
+                       onChangeText={value => setEmailVal(value.replace(/\s/g, ''))}
+                       autoCorrect={false}
+                       autoCapitalize='none'
+                       style={styles.signUpInputFields}/>
+            {
+                emailEmpty &&
+                <Text style={{color: "red"}}>Email field is required</Text>
+            }
+            <TextInput placeholder={'Password'}
+                       onChangeText={value => setPassVal(value)}
+                       autoCorrect={false}
+                       secureTextEntry={true}
+                       style={styles.signUpInputFieldss}/>
+            {
+                passwordEmpty &&
+                <Text style={{color: "red"}}>Password field is required</Text>
+            }
+            <TouchableOpacity onPress={() => this.signIn()}
                               style={styles.signButton}>
                 <Text style={styles.logInButtonTextss}>LOG IN</Text>
             </TouchableOpacity>
             <TouchableOpacity onPress={() => setShow(null)} style={styles.signButton}>
                 <Text style={styles.logInButtonTextss}>Back</Text>
             </TouchableOpacity>
+            <Text>{message}</Text>
         </SafeAreaView>
     )
 }
