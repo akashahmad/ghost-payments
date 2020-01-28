b0cd2d54d5bfa7ede70bf545bfbf069617caeda1
Cathy Minutillo <cathyminutillo@Cathys-MBP.attlocal.net>
2020-01-27 18:56:15 -0800

    changes

--- android/app/src/main/java/com/ghostpayments/MainApplication.java
+++ android/app/src/main/java/com/ghostpayments/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.github.reactnativecommunity.location.RNLocationPackage;
 import com.horcrux.svg.SvgPackage;
 import fr.bamlab.rnimageresizer.ImageResizerPackage;
 import com.imagepicker.ImagePickerPackage;
